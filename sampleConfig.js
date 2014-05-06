9769bc96363de392253c79281bf3350a72300594
Matthew Lewis <matt@mplewis.com>
2014-05-06 06:47:14 -0500

    Add sample config; ignore any actual user configs

---
+++ sampleConfig.js
@@ -0,0 +1,24 @@
+var global = {
+  domain: 'http://subdomain.yourdomain.com/',
+  secretUrlSuffix: 'putSomeAlphanumericSecretCharsHere'
+};
+
+var projects = [
+  {
+    repo: {
+      owner: 'yourbitbucketusername',
+      slug: 'your-bitbucket-repo-name',
+      sshPrivKeyPath: '/home/youruser/.ssh/id_rsa'
+    },
+    dest: {
+      host: 'yourstaticwebhost.com',
+      username: 'yourusername',
+      password: 'yourpassword',
+      path: '/home/youruser/public_html/'
+    }
+  }
+];
+
+module.exports = {
+  projects: projects
+};
