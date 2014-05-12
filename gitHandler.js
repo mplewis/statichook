078815245bb128d04ca5d1c65eeb6331dbfc6c06
Matthew Lewis <matt@mplewis.com>
2014-05-12 05:10:39 -0500

    Improve log lines for private/public clones

--- gitHandler.js
+++ gitHandler.js
@@ -7,7 +7,13 @@ function cloneInto(repoUrl, destPath, keyPath, callback) {
   var gitClone = function(keyData) {
     // clone with . to avoid the extra containing folder
     var gitCmd = 'git clone ' + repoUrl + ' .';
-    winston.log('info', 'Cloning', repoUrl);
+    // If keyData is truthy, we're using a key to clone a private repo;
+    // otherwise, we're cloning a public repo and don't need a key
+    if (keyData) {
+      winston.log('info', 'Cloning', repoUrl, 'with private key');
+    } else {
+      winston.log('info', 'Cloning', repoUrl);
+    }
     winston.log('debug', 'Running git command:', gitCmd);
     gitane.run(destPath, keyData, gitCmd, function(err, stdout, stderr, exitCode) {
       callback(err, stdout, stderr, exitCode);
