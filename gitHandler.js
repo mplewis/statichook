4da3b5ca6e78c3ec79f7914ebcb3c2d1e37d4c53
Matthew Lewis <matt@mplewis.com>
2014-05-12 02:55:55 -0500

    Move lines into fn

--- gitHandler.js
+++ gitHandler.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 var gitane = require('gitane');
 
 function cloneInto(repoUrl, destPath, keyPath, callback) {
-  // clone with . to avoid the extra containing folder
-  var gitCmd = 'git clone ' + repoUrl + ' .';
   console.log(gitCmd);
   
   var gitClone = function(keyData) {
+    // clone with . to avoid the extra containing folder
+    var gitCmd = 'git clone ' + repoUrl + ' .';
     gitane.run(destPath, keyData, gitCmd, function(err, stdout, stderr, exitCode) {
       callback(err, stdout, stderr, exitCode);
     });
