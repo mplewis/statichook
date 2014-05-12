bcad6341b9d6bf85712de73bbe8e19990d174633
Matthew Lewis <matt@mplewis.com>
2014-05-12 02:20:52 -0500

    Add a public repo example

--- sampleConfig.js
+++ sampleConfig.js
@@ -8,8 +8,22 @@ var projects = [
   {
     repo: {
       owner: 'yourbitbucketusername',
-      slug: 'your-bitbucket-source-repo-name',
+      slug: 'your-bitbucket-private-repo-name',
+      url: 'git@bitbucket.org:yourbitbucketusername/your-bitbucket-private-repo-name.git',
       sshPrivKeyPath: '/home/youruser/.ssh/id_rsa'
+    },
+    dest: {
+      host: 'yourstaticwebhost.com',
+      username: 'yourusername',
+      password: 'yourpassword',
+      path: '/home/youruser/html_dest/'
+    }
+  },
+  {
+    repo: {
+      owner: 'yourbitbucketusername',
+      slug: 'your-bitbucket-public-repo-name',
+      url: 'git@bitbucket.org:yourbitbucketusername/your-bitbucket-public-repo-name.git',
     },
     dest: {
       host: 'yourstaticwebhost.com',
