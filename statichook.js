3b5db3e0db55117ba3ed9b3540619a7d4b729cb1
Matthew Lewis <matt@mplewis.com>
2014-05-12 01:24:18 -0500

    Add schema and body parsing

--- statichook.js
+++ statichook.js
@@ -1,22 +1,44 @@
 // local packages
 var config = require('./config');
 var response = require('./response');
+var hookHandler = require('./hookHandler');
 
 // npm packages
 var express = require('express');
+var bodyParser = require('body-parser');
+var JaySchema = require('jayschema');
 
 // node packages
 var util = require('util');
 
+// set up webhook data validator
+var js = new JaySchema();
+var schema = {
+  bitbucket: require('./schemas/bitbucket.json')
+};
+
 // set up Express
 var app = express();
+app.use(bodyParser());
 
 // hooks should post to this Express address
 var postAddress = '/' + config.global.secretUrlSuffix;
 
-// set up route
+// set up speak route
 app.get(postAddress, function(req, res){
   res.send(response.speak);
+});
+
+// set up webhook route
+app.post(postAddress, function(req, res){
+  var posted = req.body;
+  var errors = js.validate(posted, schema.bitbucket);
+  if (errors.length > 0) {
+    res.send(400, errors);
+  } else {
+    res.send(204);
+    hookHandler.handle(posted);
+  }
 });
 
 // set up 403 for all non-hook addresses
