0f6e38848f114ffe85e59a3c472c22c40faadd95
Matthew Lewis <matt@mplewis.com>
2014-05-13 05:37:00 -0500

    Add example configs for projects with mkdir -p dirs and projects with repo paths

--- sampleConfig.js
+++ sampleConfig.js
@@ -29,7 +29,21 @@ var projects = [
       host: 'yourstaticwebhost.com',
       username: 'yourusername',
       password: 'yourpassword',
-      path: '/home/youruser/html_dest/'
+      path: '/home/youruser/some_other/directory/that_may/not_exist_yet/'
+    }
+  },
+  {
+    repo: {
+      owner: 'yourbitbucketusername',
+      slug: 'your-bitbucket-repo-with-subdir',
+      url: 'git@bitbucket.org:yourbitbucketusername/your-bitbucket-repo-with-subdir.git',
+      path: '/your-repo-subdir'
+    },
+    dest: {
+      host: 'yourstaticwebhost.com',
+      username: 'yourusername',
+      privateKey: '/home/youruser/.ssh/another_id_rsa',
+      path: '/home/youruser/another_dest/'
     }
   }
 ];
